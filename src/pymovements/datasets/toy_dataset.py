826fa93c4a1cb16233ce65c572a07310f3098954
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-10-13 15:25:12 +0200

    chore: update to new URL

--- src/pymovements/datasets/toy_dataset.py
+++ src/pymovements/datasets/toy_dataset.py
@@ -118,7 +118,7 @@ class ToyDataset(DatasetDefinition):
             [
                     {
                         'content': 'gaze',
-                        'url': 'http://github.com/aeye-lab/pymovements-toy-dataset/zipball/6cb5d663317bf418cec0c9abe1dde5085a8a8ebd/',  # noqa: E501 # pylint: disable=line-too-long
+                        'url': 'http://github.com/pymovements/pymovements-toy-dataset/zipball/6cb5d663317bf418cec0c9abe1dde5085a8a8ebd/',  # noqa: E501 # pylint: disable=line-too-long
                         'filename': 'pymovements-toy-dataset.zip',
                         'md5': '4da622457637a8181d86601fe17f3aa8',
                         'filename_pattern': r'trial_{text_id:d}_{page_id:d}.csv',
