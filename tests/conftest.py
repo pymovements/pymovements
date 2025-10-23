fe7a0732b6fb3f4c069fb163782ac3625408cc63
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2025-10-23 13:56:55 +0200

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Fixtures for datasets."""
+"""Fixtures for datasets. """
 
 pytest_plugins = [
     'tests.fixtures.file_fixtures',
