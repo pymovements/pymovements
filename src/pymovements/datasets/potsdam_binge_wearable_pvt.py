ecc88d52b2a5218d07301ba9a6fda11a7eed2014
Paul Prasse <prasse.paul@googlemail.com>
2026-01-20 17:17:36 +0100

    dataset: improve potsdam binge dataset definitions (#1425)

--- src/pymovements/datasets/potsdam_binge_wearable_pvt.py
+++ src/pymovements/datasets/potsdam_binge_wearable_pvt.py
@@ -137,7 +137,7 @@ class PotsdamBingeWearablePVT(DatasetDefinition):
                 {
                     'content': 'gaze',
                     'url': 'https://osf.io/download/9vbs8/',
-                    'filename': 'a.zip',
+                    'filename': 'alcohol.zip',
                     'md5': '87c6c74a9a17cbd093b91f9415e8dd9d',
                     'filename_pattern': r'{subject_id:d}_{session_id:d}_{condition:s}_{trial_id:d}_{block_id:d}.csv',  # noqa: E501 # pylint: disable=line-too-long
                     'filename_pattern_schema_overrides': {
@@ -149,7 +149,9 @@ class PotsdamBingeWearablePVT(DatasetDefinition):
                         'time_column': 'eyelink_timestamp',
                         'time_unit': 'ms',
                         'distance_column': 'target_distance',
-                        'pixel_columns': ['x_pix_eyelink', 'y_pix_eyelink'],
+                        'pixel_columns': [
+                            'x_pix_pupilcore_interpolated', 'y_pix_pupilcore_interpolated',
+                        ],
                         'read_csv_kwargs': {
                             'schema_overrides': {
                                 'trial_id': pl.Float32,
@@ -175,7 +177,7 @@ class PotsdamBingeWearablePVT(DatasetDefinition):
                 {
                     'content': 'gaze',
                     'url': 'https://osf.io/download/yqukn/',
-                    'filename': 'b.zip',
+                    'filename': 'baseline.zip',
                     'md5': '54038547b1a373253b38999a227dde63',
                     'filename_pattern': r'{subject_id:d}_{session_id:d}_{condition:s}_{trial_id:d}_{block_id:d}.csv',  # noqa: E501 # pylint: disable=line-too-long
                     'filename_pattern_schema_overrides': {
@@ -215,7 +217,7 @@ class PotsdamBingeWearablePVT(DatasetDefinition):
                 {
                     'content': 'gaze',
                     'url': 'https://osf.io/download/yf2xa/',
-                    'filename': 'e.zip',
+                    'filename': 'enrollment.zip',
                     'md5': 'a0d0203cbb273f6908c1b52a42750551',
                     'filename_pattern': r'{subject_id:d}_{session_id:d}_{condition:s}_{trial_id:d}_{block_id:d}.csv',  # noqa: E501 # pylint: disable=line-too-long
                     'filename_pattern_schema_overrides': {
@@ -224,6 +226,12 @@ class PotsdamBingeWearablePVT(DatasetDefinition):
                         'block_id': int,
                     },
                     'load_kwargs': {
+                        'time_column': 'eyelink_timestamp',
+                        'time_unit': 'ms',
+                        'distance_column': 'target_distance',
+                        'pixel_columns': [
+                            'x_pix_pupilcore_interpolated', 'y_pix_pupilcore_interpolated',
+                        ],
                         'read_csv_kwargs': {
                             'schema_overrides': {
                                 'trial_id': pl.Float32,
