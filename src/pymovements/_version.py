cfb09fd6997f1760725c079761a32b902d483e09
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2026-01-28 11:13:57 +0100

    emit warning instead of raising an exception on not installed

--- src/pymovements/_version.py
+++ src/pymovements/_version.py
@@ -1,8 +1,14 @@
 """Module for inferring pymovements version."""
+import warning
 from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version("pymovements")
-except PackageNotFoundError as e:
-    e.add_note('Please install pymovements with your package manager')
-    raise
+except PackageNotFoundError as exception:
+    exception.add_note(
+        'Inferring pymovements version failed. '
+        'Please install pymovements with your package manager.',
+    )
+    warning = RuntimeWarning(*exception.args)
+    warning.with_traceback(exception.__traceback__)
+    __version__ = 'unknown'
