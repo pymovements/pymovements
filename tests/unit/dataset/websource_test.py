da8e99874a49efc90194d1a087e42ab85e7d13be
Daniel G. Krakowczyk <daniel@krakowczyk.tech>
2026-02-24 17:38:49 +0100

    Update header in websource_test.py

--- tests/unit/dataset/websource_test.py
+++ tests/unit/dataset/websource_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025-2026 The pymovements Project Authors
+# Copyright (c) 2023-2026 The pymovements Project Authors
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -17,7 +17,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Tests for WebSource and download utilities (moved from _utils._downloads)."""
+"""Tests for WebSource and download utilities."""
 import hashlib
 import os.path
 from pathlib import Path
