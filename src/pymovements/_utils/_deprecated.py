227ee71c679c245ba95709af298dae9733c4c717
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2026-03-16 14:33:14 +0100

    remove mypy ignores. ruff format messes that up and its configuration must be changed.

--- src/pymovements/_utils/_deprecated.py
+++ src/pymovements/_utils/_deprecated.py
@@ -72,9 +72,9 @@ class DeprecatedMetaClass(type):
         if alias is not None:
 
             def new(cls: type, *args: Any, **kwargs: Any) -> type:
-                alias = cls._DeprecatedMetaClass__alias  # type: ignore[attr-defined]
-                version_deprecated = cls._DeprecatedMetaClass__version_deprecated  # type: ignore[attr-defined]
-                version_removed = cls._DeprecatedMetaClass__version_removed  # type: ignore[attr-defined]
+                alias = cls._DeprecatedMetaClass__alias
+                version_deprecated = cls._DeprecatedMetaClass__version_deprecated
+                version_removed = cls._DeprecatedMetaClass__version_removed
 
                 warn(
                     f'{cls.__name__} has been renamed to {alias.__name__} '
@@ -118,7 +118,7 @@ class DeprecatedMetaClass(type):
         """
         if subclass is cls:
             return True
-        return issubclass(subclass, cls._DeprecatedMetaClass__alias)  # type: ignore[attr-defined]
+        return issubclass(subclass, cls._DeprecatedMetaClass__alias)
 
     def __instancecheck__(cls, instance: Any) -> bool:
         """Check if is instance of deprecated class.
