7d02ec07d4027a14bc679cba8e1360ea64683a1f
Daniel G, Krakowczyk <daniel.krakowczyk@uni-potsdam.de>
2026-07-07 15:40:20 +0200

    use make_example_file instead of make_text_file to hopefully pass tests on windows

--- tests/unit/dataset/websource_test.py
+++ tests/unit/dataset/websource_test.py
@@ -618,34 +618,34 @@ def test_websource_download_first_of_two_mirrors_gaze_fails(
     ])
 
 
-def test_websource_checksum(make_text_file):
-    path = make_text_file('test.txt', 'test')
+def test_websource_checksum(make_example_file):
+    path = make_example_file('monocular_example.csv')
 
     result = WebSource.checksum(path)
 
-    assert result == 'd8e8fca2dc0f896fd7cb4cb0031ba249'
+    assert result == 'b56e208f6442bab645789defe865183b'
 
 
-def test_websource_verify_checksum_success(make_text_file):
-    path = make_text_file('test.txt', 'test')
+def test_websource_verify_checksum_success(make_example_file):
+    path = make_example_file('monocular_example.csv')
 
-    WebSource(url='test', md5='d8e8fca2dc0f896fd7cb4cb0031ba249').verify_checksum(path)
+    WebSource(url='test', md5='b56e208f6442bab645789defe865183b').verify_checksum(path)
 
 
-def test_websource_verify_checksum_mismatch(make_text_file):
-    path = make_text_file('test.txt', 'test')
+def test_websource_verify_checksum_mismatch(make_example_file):
+    path = make_example_file('monocular_example.csv')
 
     with pytest.raises(ChecksumError) as exc:
         WebSource(url='test', md5='123456').verify_checksum(path)
 
-    assert exc.value.actual == 'd8e8fca2dc0f896fd7cb4cb0031ba249'
+    assert exc.value.actual == 'b56e208f6442bab645789defe865183b'
     assert exc.value.expected == '123456'
     assert exc.value.path == path
     assert exc.value.algorithm == 'MD5'
 
 
-def test_websource_verify_checksum_no_checksum(make_text_file):
-    path = make_text_file('test.txt', 'test')
+def test_websource_verify_checksum_no_checksum(make_example_file):
+    path = make_example_file('monocular_example.csv')
 
     message = 'WebSource.md5 must be of type string but got NoneType'
 
