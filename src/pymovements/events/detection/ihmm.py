9a7a319a369e0ac86f505d501aaab54335abf1a4
Dionigi <105047564+Di0nigi@users.noreply.github.com>
2026-07-08 16:21:23 +0200

    circular import fix

--- src/pymovements/events/detection/ihmm.py
+++ src/pymovements/events/detection/ihmm.py
@@ -27,7 +27,7 @@ import numpy
 from pymovements._utils import _checks
 from pymovements.events.detection.library import register_event_detection
 from pymovements.events.events import Events
-from pymovements.gaze.transforms_numpy import norm
+
 
 
 def format_optimal_dict(opt: dict[str, Any]) -> dict[str, list[float] | list[list[float]]]:
@@ -1265,3 +1265,5 @@ def ihmm(
     events = Events(name=name, onsets=onsets_arr, offsets=offsets_arr)
 
     return events
+
+from pymovements.gaze.transforms_numpy import norm
