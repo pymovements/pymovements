0235f533c6b73793d6cb0247b0b03d341c654073
Dionigi <105047564+Di0nigi@users.noreply.github.com>
2026-07-09 12:25:59 +0200

    add import back

--- src/pymovements/events/detection/ihmm.py
+++ src/pymovements/events/detection/ihmm.py
@@ -28,7 +28,7 @@ import numpy
 from pymovements._utils import _checks
 from pymovements.events.detection.library import register_event_detection
 from pymovements.events.events import Events
-# from pymovements.gaze.transforms_numpy import norm
+from pymovements.gaze.transforms_numpy import norm
 
 
 def format_optimal_dict(opt: dict[str, Any]) -> dict[str, list[float] | list[list[float]]]:
@@ -1104,8 +1104,8 @@ def ihmm(
 
     # convert into velocities (1D velocities vector)
 
-    # velocities_1d = norm(velocities, axis=1)
-    velocities_1d = velocities.mean(axis=1)
+    velocities_1d = norm(velocities, axis=1)
+    #velocities_1d = velocities.mean(axis=1)
 
     vel_mask = ~numpy.isnan(velocities_1d)
     cW = 0
