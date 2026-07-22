ea791c200f4b004ccfc39dbe63d2c372d523a8b1
Carlson Büth <commit@cbueth.de>
2026-07-22 14:16:14 +0200

    chore: ignore sphinx ResourceDefinition links for now
    Signed-off-by: Carlson Büth <commit@cbueth.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -205,7 +205,7 @@ nitpick_ignore_regex = [
     (r'py:(attr|meth)', r'^(?:Dataset|Gaze|DatasetPaths|Experiment)\..*'),
 
     # Explicit :py:attr: cross-references to class attributes. broken until #713 is resolved
-    (r'py:attr', r'^pymovements\.(?:Dataset|Gaze|DatasetPaths|Experiment)\..*'),
+    (r'py:attr', r'^pymovements\.(?:Dataset|Gaze|DatasetPaths|Experiment|ResourceDefinition)\..*'),
 
     # Odd matplotlib reference seen in deprecated utils.plotting docs
     (r'py:class', r'^matplotlib\.pyplot\.figure$'),
